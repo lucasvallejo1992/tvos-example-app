0ac55de3d51909085b9b6fccebbb4be008dedce8
Lucas Vallejo <lucasvallejo92@gmail.com>
2023-02-01 15:04:10 -0300

    Initial Commit

---
+++ tvos-example-app/tvos_example_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  tvos_example_appApp.swift
+//  tvos-example-app
+//
+//  Created by Lucas Vallejo on 01/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct tvos_example_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
